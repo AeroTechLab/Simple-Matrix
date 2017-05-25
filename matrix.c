001ea65ca1cbe405ad85f12b4004e5ce0cab6e9a
Bitiquinho <consoni_2519@hotmail.com>
2017-05-25 07:24:20 -0300

    Add source code

---
+++ matrix.c
@@ -0,0 +1,337 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//                                                                                  //
+//  Copyright (c) 2016-2017 Leonardo Consoni <consoni_2519@hotmail.com>             //
+//                                                                                  //
+//  This file is part of Simple C Matrix Library.                                   //
+//                                                                                  //
+//  Simple C Matrix Library is free software: you can redistribute it and/or modify //
+//  it under the terms of the GNU Lesser General Public License as published        //
+//  by the Free Software Foundation, either version 3 of the License, or            //
+//  (at your option) any later version.                                             //
+//                                                                                  //
+//  Simple C Matrix Library is distributed in the hope that it will be useful,      //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of                  //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    //
+//  GNU Lesser General Public License for more details.                             //
+//                                                                                  //
+//  You should have received a copy of the GNU Lesser General Public License        //
+//  along with Simple C Matrix Library. If not, see <http://www.gnu.org/licenses/>. //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "matrix.h"
+
+
+
+// Fortran77 function declarations
+
+// (BLAS) matrix-matrix product
+extern void dgemm_( char* tA, char* tB, int* m, int* n, int* k, double* alpha, double* A, int* ldA, double* B, int* ldB, double* beta, double* C, int* ldC );  
+// (LAPACK) LU decomposition of a general matrix
+extern void dgetrf_( int* M, int *N, double* A, int* ldA, int* IPIV, int* INFO );
+// (LAPACK) generate inverse of a matrix given its LU decomposition
+extern void dgetri_( int* N, double* A, int* ldA, int* IPIV, double* WORK, int* lwork, int* INFO );
+
+
+struct _MatrixData
+{
+  double* data;
+  size_t rowsNumber, columnsNumber;
+};
+
+
+Matrix Mat_Create( double* data, size_t rowsNumber, size_t columnsNumber )
+{
+  if( rowsNumber > MATRIX_SIZE_MAX || columnsNumber > MATRIX_SIZE_MAX ) return NULL;
+
+  Matrix newMatrix = (Matrix) malloc( sizeof(MatrixData) );
+
+  newMatrix->data = (double*) calloc( rowsNumber * columnsNumber, sizeof(double) );
+
+  newMatrix->rowsNumber = rowsNumber;
+  newMatrix->columnsNumber = columnsNumber;
+
+  if( data == NULL ) Mat_Clear( newMatrix );
+  else Mat_SetData( newMatrix, data );
+
+  return newMatrix;
+}
+
+Matrix Mat_CreateSquare( size_t size, char type )
+{
+  Matrix newSquareMatrix = Mat_Create( NULL, size, size );
+
+  if( type == 'I' )
+  {
+    for( size_t line = 0; line < size; line++ )
+      newSquareMatrix->data[ line * size + line ] = 1.0;
+  }
+
+  return newSquareMatrix;
+}
+
+void Mat_Discard( Matrix matrix )
+{
+  if( matrix == NULL ) return;
+  
+  free( matrix->data );
+  
+  free( matrix );
+}
+
+Matrix Mat_Copy( Matrix source, Matrix destination )
+{
+  if( source == NULL || destination == NULL ) return NULL;
+
+  destination->rowsNumber = source->rowsNumber;
+  destination->columnsNumber = source->columnsNumber;
+
+  memcpy( destination->data, source->data, destination->rowsNumber * destination->columnsNumber * sizeof(double) );
+
+  return destination;
+}
+
+Matrix Mat_Clear( Matrix matrix )
+{
+  if( matrix == NULL ) return NULL;
+
+  memset( matrix->data, 0, matrix->rowsNumber * matrix->columnsNumber * sizeof(double) );
+
+  return matrix;
+}
+
+size_t Mat_GetWidth( Matrix matrix )
+{
+  if( matrix == NULL ) return 0;
+
+  return matrix->columnsNumber;
+}
+
+size_t Mat_GetHeight( Matrix matrix )
+{
+  if( matrix == NULL ) return 0;
+
+  return matrix->rowsNumber;
+}
+
+double Mat_GetElement( Matrix matrix, size_t row, size_t column )
+{
+  if( matrix == NULL ) return 0.0;
+
+  if( row >= matrix->rowsNumber || column >= matrix->columnsNumber ) return 0.0;
+
+  return matrix->data[ column * matrix->rowsNumber + row ];
+}
+
+void Mat_SetElement( Matrix matrix, size_t row, size_t column, double value )
+{
+  if( matrix == NULL ) return;
+
+  if( row >= matrix->rowsNumber || column >= matrix->columnsNumber ) return;
+
+  matrix->data[ column * matrix->rowsNumber + row ] = value;
+}
+
+double* Mat_GetData( Matrix matrix, double* buffer )
+{
+  if( matrix == NULL ) return NULL;
+
+  for( size_t row = 0; row < matrix->rowsNumber; row++ )
+  {
+    for( size_t column = 0; column < matrix->columnsNumber; column++ )
+      buffer[ row * matrix->columnsNumber + column ] = matrix->data[ column * matrix->rowsNumber + row ];
+  }
+  
+  return buffer;
+}
+
+void Mat_SetData( Matrix matrix, double* data )
+{
+  if( matrix == NULL ) return;
+
+  for( size_t column = 0; column < matrix->columnsNumber; column++ )
+  {
+    for( size_t row = 0; row < matrix->rowsNumber; row++ )
+      matrix->data[ column * matrix->rowsNumber + row ] = data[ row * matrix->columnsNumber + column ];
+  }
+}
+
+Matrix Mat_Resize( Matrix matrix, size_t rowsNumber, size_t columnsNumber )
+{
+  double auxArray[ MATRIX_SIZE_MAX * MATRIX_SIZE_MAX ];
+  
+  if( matrix == NULL )
+    matrix = Mat_Create( NULL, rowsNumber, columnsNumber );
+  else 
+  {
+    if( matrix->rowsNumber * matrix->columnsNumber < rowsNumber * columnsNumber )
+      matrix->data = (double*) realloc( matrix->data, rowsNumber * columnsNumber * sizeof(double) );
+  
+    memcpy( auxArray, matrix->data, matrix->rowsNumber * matrix->columnsNumber * sizeof(double) );
+    
+    memset( matrix->data, 0, rowsNumber * columnsNumber * sizeof(double) );
+    
+    for( size_t column = 0; column < matrix->columnsNumber; column++ )
+    {
+      for( size_t row = 0; row < matrix->rowsNumber; row++ )
+        matrix->data[ column * rowsNumber + row ] = auxArray[ column * matrix->rowsNumber + row ];
+    }
+    
+    matrix->rowsNumber = rowsNumber;
+    matrix->columnsNumber = columnsNumber;
+  }
+
+  return matrix;
+}
+
+Matrix Mat_Scale( Matrix matrix, double scalar, Matrix result )
+{
+  if( matrix == NULL ) return NULL;
+  
+  size_t elementsNumber = result->rowsNumber * result->columnsNumber;
+  for( size_t elementIndex = 0; elementIndex < elementsNumber; elementIndex++ )
+    result->data[ elementIndex ] = scalar * matrix->data[ elementIndex ];
+
+  result->rowsNumber = matrix->rowsNumber;
+  result->columnsNumber = matrix->columnsNumber;
+  
+  return result;
+}
+
+Matrix Mat_Sum( Matrix matrix_1, double weight_1, Matrix matrix_2, double weight_2, Matrix result )
+{
+  if( matrix_1 == NULL || matrix_2 == NULL ) return NULL;
+
+  if( matrix_1->rowsNumber != matrix_2->rowsNumber || matrix_1->columnsNumber != matrix_2->columnsNumber ) return NULL;
+
+  result->rowsNumber = matrix_1->rowsNumber;
+  result->columnsNumber = matrix_1->columnsNumber;
+  
+  size_t elementsNumber = result->rowsNumber * result->columnsNumber;
+  for( size_t elementIndex = 0; elementIndex < elementsNumber; elementIndex++ )
+    result->data[ elementIndex ] = weight_1 * matrix_1->data[ elementIndex ] + weight_2 * matrix_2->data[ elementIndex ];
+
+  return result;
+}
+
+Matrix Mat_Dot( Matrix matrix_1, char transpose_1, Matrix matrix_2, char transpose_2, Matrix result )
+{
+  const double alpha = 1.0;
+  const double beta = 0.0;
+  
+  double auxArray[ MATRIX_SIZE_MAX * MATRIX_SIZE_MAX ];
+  
+  if( matrix_1 == NULL || matrix_2 == NULL ) return NULL;
+  
+  size_t couplingLength = ( transpose_1 == MATRIX_TRANSPOSE ) ? matrix_1->rowsNumber : matrix_1->columnsNumber;
+   
+  if( couplingLength != ( ( transpose_2 == MATRIX_TRANSPOSE ) ? matrix_2->columnsNumber : matrix_2->rowsNumber ) ) return NULL;
+   
+  result->rowsNumber = ( transpose_1 == MATRIX_TRANSPOSE ) ? matrix_1->columnsNumber : matrix_1->rowsNumber;
+  result->columnsNumber = ( transpose_2 == MATRIX_TRANSPOSE ) ? matrix_2->rowsNumber : matrix_2->columnsNumber;
+  
+  int stride_1 = ( transpose_1 == MATRIX_TRANSPOSE ) ? couplingLength : result->rowsNumber;          // Distance between columns
+  int stride_2 = ( transpose_2 == MATRIX_TRANSPOSE ) ? result->columnsNumber : couplingLength;       // Distance between columns
+  
+  dgemm_( &transpose_1, &transpose_2, (int*) &(result->rowsNumber),(int*) &(result->columnsNumber), (int*) &(couplingLength), 
+          (double*) &alpha, matrix_1->data, &stride_1, matrix_2->data, &stride_2, (double*) &beta, auxArray, (int*) &result->rowsNumber );
+  
+  memcpy( result->data, auxArray, result->rowsNumber * result->columnsNumber * sizeof(double) );
+
+  return result;
+}
+
+double Mat_Determinant( Matrix matrix )
+{
+  double auxArray[ MATRIX_SIZE_MAX * MATRIX_SIZE_MAX ];
+  int pivotArray[ MATRIX_SIZE_MAX ];
+  int info;
+  
+  if( matrix == NULL ) return 0.0;
+
+  if( matrix->rowsNumber != matrix->columnsNumber ) return 0.0;
+  
+  memcpy( auxArray, matrix->data, matrix->rowsNumber * matrix->columnsNumber * sizeof(double) );
+  
+  dgetrf_( (int*) &(matrix->rowsNumber), (int*) &(matrix->columnsNumber), auxArray, (int*) &(matrix->rowsNumber), pivotArray, &info );
+  
+  double determinant = 1.0;
+  for( size_t pivotIndex = 0; pivotIndex < matrix->rowsNumber; pivotIndex++ )
+  {
+    determinant *= auxArray[ pivotIndex * matrix->rowsNumber + pivotIndex ];
+    if( pivotArray[ pivotIndex ] != pivotIndex ) determinant *= -1.0;
+  }
+
+  return determinant;
+}
+
+Matrix Mat_Transpose( Matrix matrix, Matrix result )
+{
+  double auxArray[ MATRIX_SIZE_MAX * MATRIX_SIZE_MAX ] = { 0 };
+  
+  if( matrix == NULL ) return NULL;
+
+  result->rowsNumber = matrix->columnsNumber;
+  result->columnsNumber = matrix->rowsNumber;
+
+  for( size_t row = 0; row < result->rowsNumber; row++ )
+  {
+    for( size_t column = 0; column < result->columnsNumber; column++ )
+      auxArray[ column * result->rowsNumber + row ] = matrix->data[ row * matrix->rowsNumber + column ];
+  }
+
+  memcpy( result->data, auxArray, result->rowsNumber * result->columnsNumber * sizeof(double) );
+
+  return result;
+}
+
+Matrix Mat_Inverse( Matrix matrix, Matrix result )
+{
+  double auxArray[ MATRIX_SIZE_MAX * MATRIX_SIZE_MAX ] = { 0 };
+  int pivotArray[ MATRIX_SIZE_MAX ];
+  int info;
+  
+  if( matrix == NULL || result == NULL ) return NULL;
+
+  if( matrix->rowsNumber != matrix->columnsNumber ) return NULL;
+
+  if( matrix != result )
+  {
+    result->rowsNumber = matrix->rowsNumber;
+    result->columnsNumber = matrix->columnsNumber;
+  
+    memcpy( result->data, matrix->data, matrix->rowsNumber * matrix->columnsNumber * sizeof(double) );
+  }
+  
+  dgetrf_( (int*) &(result->rowsNumber), (int*) &(result->columnsNumber), result->data, (int*) &(result->rowsNumber), pivotArray, &info );
+  
+  if( info != 0 ) return NULL;
+  
+  int workLength = result->rowsNumber * result->columnsNumber;
+  dgetri_( (int*) &(result->rowsNumber), result->data, (int*) &(result->rowsNumber), pivotArray, auxArray, &workLength, &info );
+  
+  if( info != 0 ) return NULL;
+
+  return result;
+}
+
+void Mat_Print( Matrix matrix )
+{
+  if( matrix == NULL ) return;
+
+  printf( "[%lux%lu] matrix:\n", matrix->rowsNumber, matrix->columnsNumber );
+  for( size_t row = 0; row < matrix->rowsNumber; row++ )
+  {
+    printf( "[" );
+    for( size_t column = 0; column < matrix->columnsNumber; column++ )
+      printf( " %.6f", matrix->data[ column * matrix->rowsNumber + row ] );
+    printf( " ]\n" );
+  }
+  printf( "\n" );
+}
---
+++ matrix.h
@@ -0,0 +1,160 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//                                                                                  //
+//  Copyright (c) 2016-2017 Leonardo Consoni <consoni_2519@hotmail.com>             //
+//                                                                                  //
+//  This file is part of Simple C Matrix Library.                                   //
+//                                                                                  //
+//  Simple C Matrix Library is free software: you can redistribute it and/or modify //
+//  it under the terms of the GNU Lesser General Public License as published        //
+//  by the Free Software Foundation, either version 3 of the License, or            //
+//  (at your option) any later version.                                             //
+//                                                                                  //
+//  Simple C Matrix Library is distributed in the hope that it will be useful,      //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of                  //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    //
+//  GNU Lesser General Public License for more details.                             //
+//                                                                                  //
+//  You should have received a copy of the GNU Lesser General Public License        //
+//  along with Simple C Matrix Library. If not, see <http://www.gnu.org/licenses/>. //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+/// @file matrix.h
+/// @brief Matrix data structure and operations abstractions
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <stdint.h>
+
+#define MATRIX_SIZE_MAX 50          ///< Maximum allowed matrix dimension (rows or columns number)
+
+#define MATRIX_IDENTITY 'I'         ///< Create square matrix as identity type (main diagonal filled with 1's)
+#define MATRIX_ZERO '0'             ///< Create square matrix as zero type (completely zeroed)
+
+#define MATRIX_TRANSPOSE 'T'        ///< Transpose matrix before multiplication
+#define MATRIX_KEEP 'N'             ///< Keep matrix unadulterated before multiplication
+
+
+typedef struct _MatrixData MatrixData;    ///< Matrix internal data structure
+typedef MatrixData* Matrix;               ///< Opaque reference to Matrix data structure
+
+
+/// @brief Creates matrix with specified values and dimensions                                               
+/// @param[in] data array with values in row-major order to fill matrix data (NULL for filling with zeros)                                 
+/// @param[in] rowsNumber number of rows                                         
+/// @param[in] columnsNumber number of columns      
+/// @return reference/pointer to allocated and filled matrix (NULL if dimensions are bigger than MATRIX_SIZE_MAX)
+Matrix Mat_Create( double* data, size_t rowsNumber, size_t columnsNumber );     
+
+/// @brief Creates square matrix of specified size and type                              
+/// @param[in] size size/order of the square matrix (equal number of rows and cells)
+/// @param[in] type defines if internal data is filled as zero (MATRIX_ZERO) or identity (MATRIX_IDENTITY) matrix       
+/// @return reference/pointer to allocated and filled matrix (NULL if size is bigger than MATRIX_SIZE_MAX)
+Matrix Mat_CreateSquare( size_t size, char type );
+
+/// @brief Destroys/deallocates memory of matrix 
+/// @param[in] matrix reference to matrix to be destroyed/deallocated
+void Mat_Discard( Matrix matrix );
+                                                                      
+/// @brief Copies content from one matrix to another, previously allocated  
+/// @param[in] source reference to matrix from which data will be copied
+/// @param[in] destination matrix to which data will be copied
+/// @return reference/pointer to destination matrix (NULL on errors)
+Matrix Mat_Copy( Matrix source, Matrix destination );
+
+/// @brief Sets all elements of given matrix to zero                             
+/// @param[in] matrix reference to matrix to be cleared/zeroed
+/// @return reference/pointer to cleared matrix
+Matrix Mat_Clear( Matrix matrix );
+
+/// @brief Gets columns number for given matrix                   
+/// @param[in] matrix reference to matrix
+/// @return number of columns for the matrix (0 on errors)
+size_t Mat_GetWidth( Matrix matrix );
+
+/// @brief Gets rows number for given matrix                        
+/// @param[in] matrix reference to matrix
+/// @return number of rows for the matrix (0 on errors)
+size_t Mat_GetHeight( Matrix matrix );
+
+/// @brief Gets value of given matrix element at specified position                              
+/// @param[in] matrix reference to matrix
+/// @param[in] row row position of accessed element                             
+/// @param[in] column column position of accessed element
+/// @return value of specified element (0 on error)
+double Mat_GetElement( Matrix matrix, size_t row, size_t column );
+                                                                            
+/// @brief Sets value of given matrix element at specified position 
+/// @param[in] matrix reference to matrix
+/// @param[in] row row position of accessed element                            
+/// @param[in] column column position of accessed element                                            
+/// @param[in] value new value of updated element
+void Mat_SetElement( Matrix matrix, size_t row, size_t column, double value );
+
+/// @brief Gets value of given matrix element at specified position                         
+/// @param[in] matrix reference to matrix
+/// @param[out] buffer reference
+/// @return pointer to filled buffer (NULL on errors)
+double* Mat_GetData( Matrix matrix, double* buffer );
+
+/// @brief Sets given matrix values from row-major order data array                          
+/// @param[in] matrix reference to matrix
+/// @param[in] data row-major order data array for filling the matrix
+void Mat_SetData( Matrix matrix, double* data );
+
+/// @brief Resizes/reallocates given matrix to specified dimensions. Fill new space with zeros when growing                    
+/// @param[in] matrix reference to matrix to be resized
+/// @param[in] rowsNumber new number of rows
+/// @param[in] columnsNumber new number of columns
+/// @return reference/pointer to resized/reallocated matrix (NULL on errors)
+Matrix Mat_Resize( Matrix matrix, size_t rowsNumber, size_t columnsNumber );
+
+/// @brief Multiply all given matrix elements by a specified value                              
+/// @param[in] matrix reference to matrix to be scaled
+/// @param[in] factor scalar value that multiplies the matrix
+/// @param[in] result preallocated matrix to store the scaling result (can be the same as the input one)
+/// @return reference/pointer to @a result scaled matrix (NULL on errors)
+Matrix Mat_Scale( Matrix matrix, double factor, Matrix result );
+
+/// @brief Defines desired internal count for given semaphore
+/// @param[in] matrix_1 reference to first matrix
+/// @param[in] weight_1 weight of first matrix on sum
+/// @param[in] matrix_2 reference to second matrix
+/// @param[in] weight_2 weight of second matrix on sum
+/// @param[in] result preallocated matrix to store the sum result
+/// @return reference/pointer to sum @a result matrix (NULL on errors)
+Matrix Mat_Sum( Matrix matrix_1, double weight_1, Matrix matrix_2, double weight_2, Matrix result );
+
+/// @brief Perform dot product/multiplication of 2 matrices
+/// @param[in] matrix_1 reference to first matrix (nxk dimensions after transformation)
+/// @param[in] trans_1 defines transformation applied to first matrix (MATRIX_TRANSPOSE or MATRIX_KEEP) 
+/// @param[in] matrix_2 reference to second matrix (kxm dimensions after transformation) 
+/// @param[in] trans_2 defines transformation applied to second matrix (MATRIX_TRANSPOSE or MATRIX_KEEP) 
+/// @param[in] result preallocated matrix to store the dot product/multiplication result (nxm dimensions)
+/// @return reference/pointer to multiplication @a result matrix (NULL on errors)
+Matrix Mat_Dot( Matrix matrix_1, char trans_1, Matrix matrix_2, char trans_2, Matrix result );
+
+/// @brief Calculates determinant of given matrix
+/// @param[in] matrix reference to matrix
+/// @return determinant value (0.0 on errors)
+double Mat_Determinant( Matrix matrix );
+
+/// @brief Transposes given matrix
+/// @param[in] matrix reference to matrix to be trasnposed (nxm dimensions)
+/// @param[in] result preallocated matrix (n*m data size) to store the transposition result (can be the same as the input one)
+/// @return reference/pointer to transposed @a result matrix (NULL on errors)
+Matrix Mat_Transpose( Matrix matrix, Matrix result );
+
+/// @brief Calculate inverse of given square matrix                             
+/// @param[in] matrix reference to matrix to be inverted (should be square)
+/// @param[in] result preallocated matrix to store the inversion result (can be the same as the input one)
+/// @return reference/pointer to inverted @a result matrix (NULL on errors)
+Matrix Mat_Inverse( Matrix matrix, Matrix result );
+
+/// @brief Print given matrix element values in a formatted way                             
+/// @param[in] matrix reference to matrix to be displayed
+void Mat_Print( Matrix matrix );
+
+#endif // MATRICES_H
